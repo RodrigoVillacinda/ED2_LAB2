d011f5e88726caea9e17785856e8ba7e8e0b0ba7
rodrigo alejandro <ligoche13@hotmail.com>
2018-10-08 21:38:52 -0600

    Compresion

--- app/src/main/java/ZigZag/ZigZag.java
+++ app/src/main/java/ZigZag/ZigZag.java
@@ -20,6 +20,7 @@ public class ZigZag {
         return resultado;
     }
 
+    //devuelva la cantidad de olas que existen
     private int NumeroOlas(){
         int resultado=0;
         resultado = TamañoTexto()/TamañoOla();
@@ -35,7 +36,7 @@ public class ZigZag {
     }
 
     private int TamañoTexto(){
-        int resultado=1;
+        int resultado=0;
         char TamañoTexto[]=Texto.toCharArray();
         for (int i=0;i<TamañoTexto.length; i++)
         {
@@ -45,15 +46,110 @@ public class ZigZag {
 
     }
 
-    private List ContediooLAS(){
-        List<Character> ListaResultado = new ArrayList<Character>();
-        char TamañoTexto[]=Texto.toCharArray();
-        char a=' ';
-        for (int i=0; i < TamañoOla(); i++){
-            ListaResultado=(TamañoTexto[i]==' ')? ListaResultado.add(): ListaResultado.add(TamañoTexto[i]);
+    private List<Character> ListaCaracteresTotales(){
+        List<Character> lista = new ArrayList<Character>();
+        char caracteres[]= Texto.toCharArray();
+
+        for (int i=0;i<TamañoTexto(); i++){
+            lista.add(caracteres[i]);
         }
+
+        return  lista;
+    }
+
+    public List ContedionoLista(){
+
+        List<Character> ListaResultado = new ArrayList<Character>();
+        char TamañoText[]=Texto.toCharArray();
+        char res=' ';
+
+        if (!ListaCaracteresTotales().isEmpty()) {
+            for (int i = 0; i < TamañoOla(); i++) {
+                res = ListaCaracteresTotales().remove(i);
+                ListaResultado.add(res);
+            }
+        }
+        else {
+
+            ListaResultado.add('1');
+        }
+
         return ListaResultado;
     }
 
 
+    public List<List<Character>> ListasCompresion(){
+        String TextoCompreso="";
+
+        List<List<Character>> ListasOlas=new ArrayList<>();
+
+        char TamañoText[]=Texto.toCharArray();
+        char res=' ';
+
+        List<Character> Temporal= ListaCaracteresTotales();
+
+
+        for (int i=0; i<=NumeroOlas(); i++){ //5
+            List<Character> ListaPrueba = new ArrayList<>();
+
+            if (!Temporal.isEmpty()) {
+                for (int j = 0; j <TamañoOla()  ; j++) {
+
+                    res = Temporal.remove(0);
+                    ListaPrueba.add(res);
+                    if (Temporal.isEmpty()){
+                        int tamaño=TamañoOla()-ListaPrueba.size();
+                        for (int h=0; h < tamaño; h++){
+                            ListaPrueba.add('|');
+
+                        }
+                        //ListasOlas.add(ListaPrueba);
+                        j=TamañoOla()+1;
+                    }
+
+                }
+
+                ListasOlas.add(ListaPrueba);
+                //ListaPrueba.clear();
+            }
+            else {
+
+                ListaPrueba.add('1');
+            }
+
+
+        }
+
+
+
+        return ListasOlas;
+    }
+
+    public String Compresion(){
+
+        String caracter="";
+        String compresion="";
+        List<String> Litaaa= new ArrayList<>();
+
+
+        for (int i=0; i <=ListasCompresion().get(0).size()-1; i++ ){
+
+            caracter = ListasCompresion().get(0).get(i).toString() + ListasCompresion().get(1).get(i).toString() +
+                    ListasCompresion().get(2).get(i).toString() + ListasCompresion().get(3).get(i).toString()+
+            ListasCompresion().get(4).get(i).toString();
+
+
+            Litaaa.add(caracter);
+        }
+
+        compresion = Litaaa.toString();
+        for (int i=0;i<Litaaa.size(); i++){
+            compresion=ListasCompresion().get(i).toString();
+        }
+
+        return compresion;
+    }
+
 }
+
+
--- app/src/main/java/com/example/t213/ed2_lab2/MainActivity.java
+++ app/src/main/java/com/example/t213/ed2_lab2/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.t213.ed2_lab2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import ZigZag.ZigZag;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ZigZag hola= new ZigZag(3,"Prueba de zig zag");
+        hola.ContedionoLista();
+        hola.Compresion();
     }
 }
