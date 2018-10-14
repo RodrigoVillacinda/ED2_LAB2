f879ea4efca8595b3ae4c0b354d25b4aae530b6a
rodrigo alejandro <ligoche13@hotmail.com>
2018-10-14 15:40:21 -0600

    LS-2 completado

--- app/src/main/java/S_DES/CifradoSDES.java
+++ app/src/main/java/S_DES/CifradoSDES.java
@@ -33,4 +33,68 @@ public class CifradoSDES {
         //Salida: 3,5,2,7,4,10,1,9,8,6
     }
 
+    //divide la llave en dos mitades+
+    //entrada:{10000} | {01100}
+    //salida: {00001} | {11000}
+    private int[] LS_1(){
+
+        int ls_1[] = new int[10];
+        int left[] = new int[5]; //{10000}
+        int right[] = new int[5]; //{01100}
+
+        ls_1[0] = TenPermutations()[4]; //3
+        ls_1[1] = TenPermutations()[3]; //5
+        ls_1[2] = TenPermutations()[2]; //2
+        ls_1[3] = TenPermutations()[0]; //7
+        ls_1[4] = TenPermutations()[1]; //4
+
+        ls_1[5] = TenPermutations()[9]; //10
+        ls_1[6] = TenPermutations()[6]; //1
+        ls_1[7] = TenPermutations()[7]; //9
+        ls_1[8] = TenPermutations()[8]; //8
+        ls_1[9] = TenPermutations()[5]; //6
+
+        return ls_1;
+    }
+
+    //Entrada: {00001} | {11000}
+    //salida: 10100100
+    private int[] EightPrermutatiosn(){
+        int eightpermutations[] = new int[8];
+
+        eightpermutations[0] = TenPermutations()[5];
+        eightpermutations[1] = TenPermutations()[2];
+        eightpermutations[2] = TenPermutations()[3];
+        eightpermutations[3] = TenPermutations()[7];
+        eightpermutations[4] = TenPermutations()[4];
+        eightpermutations[5] = TenPermutations()[9];
+        eightpermutations[6] = TenPermutations()[8];
+
+        return eightpermutations;
+    }
+
+    //Entrada: {00001} | {11000}
+    //Salida: {00100} | {00011}
+    private int[] LS_2(){
+
+        int ls_2[] = new int[10];
+        int left[] = new int[5]; //{10000}
+        int right[] = new int[5]; //{01100}
+
+        ls_2[0] = LS_1()[2]; //3
+        ls_2[1] = LS_1()[3]; //5
+        ls_2[2] = LS_1()[4]; //2
+        ls_2[3] = LS_1()[0]; //7
+        ls_2[4] = LS_1()[1]; //4
+
+        ls_2[5] = LS_1()[7]; //10
+        ls_2[6] = LS_1()[8]; //1
+        ls_2[7] = LS_1()[9]; //9
+        ls_2[8] = LS_1()[5]; //8
+        ls_2[9] = LS_1()[6]; //6
+
+        return ls_2;
+    }
+
+
 }
