dca05a7e30ae604f0233cecfeba4386e58bfbd4c
Dennis García <dennisgarcialg2@gmail.com>
2018-10-17 10:12:12 -0600

    Inicio
    Se agregó la activity inicio para poder seleccionar los distintos tipos de cifrado, diferenciando los procedimientos

---
+++ app/src/main/java/com/example/t213/ed2_lab2/Inicio.java
@@ -0,0 +1,17 @@
+package com.example.t213.ed2_lab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Inicio extends AppCompatActivity {
+
+    Button btnZigZag;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
